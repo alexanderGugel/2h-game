a7dd59749fbbe31388bb964399b45b085d03d8db
Alexander Gugel <alexander.gugel@gmail.com>
2014-06-19 21:29:32 -0700

    Finish basic requirements

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 var safe = false;
+var inCab = false;
 
 var checkConflict = function () {
-  if ((player.x + 20) <= (car.x + 80) && player.x >= car.x) {
+  if ((player.x + 20) <= (car.x + 80) && player.x >= car.x && !safe && !inCab) {
     $('.dead').show();
   }
 };
 
 var checkPolice = function () {
   if ((player.x + 20) <= (police.x + 80) && player.x >= police.x) {
-    safe = true;
-    $('.status').text('You are safe (for 2s).');
-    setTimeout(function () {
-      safe = false;
-      $('.status').text('Your are not safe anymore.');
-    }, 2000);
+    if (!safe && !inCab) {
+      safe = true;
+      $('.status').text('You are safe (for 2s).');
+      setTimeout(function () {
+        safe = false;
+        $('.status').text('Your are not safe anymore.');
+      }, 2000);
+    }
   }
 };
 
 var checkCab = function () {
-
+  if ((player.x + 20) <= (cab.x + 80) && player.x >= cab.x) {
+    if (!inCab && !safe) {
+      inCab = true;
+      $('.status').text('You entered a cab (for 3s).');
+      $('.player').hide();
+      setTimeout(function () {
+        player.x = cab.x;
+        $('.player').css('margin-left', player.x);
+        $('.player').show();
+        $('.status').text('Your left the cab.');
+      }, 3000);
+      setTimeout(function () {
+        inCab = false;
+      }, 5000);
+    }
+  }
 };
-
 
 var player = {
   x: Math.floor(window.innerWidth/2),
@@ -47,9 +64,7 @@ var car = {
       duration: 2000,
       step: function (x) {
         car.x = x;
-        if (!safe) {
-          checkConflict();
-        }
+        checkConflict();
       }
     });
   }
@@ -72,7 +87,7 @@ var cab = {
   render: function () {
     this.x = Math.floor(Math.random()*window.innerWidth);
     $('.cab').animate({'margin-left': this.x + 'px'}, {
-      duration: 2000,
+      duration: 1000,
       step: function (x) {
         cab.x = x;
         checkCab();
@@ -81,14 +96,17 @@ var cab = {
   }
 };
 
+
 $('body').keydown(function(e) {
-  if (e.keyCode == 37) { // left
-    player.moveLeft();
-  } else if (e.keyCode == 39) { // right
-    player.moveRight();
+  if (!inCab) {
+    if (e.keyCode == 37) { // left
+      player.moveLeft();
+    } else if (e.keyCode == 39) { // right
+      player.moveRight();
+    }
   }
 });
 
 setInterval(car.render, 2000);
 setInterval(police.render, 2000);
-setInterval(cab.render, 2000);
+setInterval(cab.render, 3000);
